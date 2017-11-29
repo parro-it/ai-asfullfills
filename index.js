e4fa95102b10df09ea596fac5f7cdd27358ff3a3
Andrea Parodi <andrea@parro.it>
2017-11-29 20:43:19 +0100

    accept a promise of iterable

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import AsyncIterable from "asynciterable";
+import isPromise from "is-promise";
 
 export default function aiAsfullfills(data) {
-  return new AsyncIterable((write, end, error) => {
+  return new AsyncIterable(async (write, end, error) => {
+    if (isPromise(data)) {
+      data = await data;
+    }
     const allPromises = data.map(p => p.then(write).catch(error));
     Promise.all(allPromises).then(() => end());
   });
--- test.js
+++ test.js
@@ -2,8 +2,15 @@ import test from "tape-async";
 import delay from "delay";
 import asFullfills from ".";
 import concat from "ai-concat";
-test("exports a function", async t => {
+
+test("emit promise values in the oprder they are fullfilled", async t => {
   const iterable = [3, 1, 2, 6, 5].map(v => delay(v * 10).then(() => v));
   const result = await concat.obj(asFullfills(iterable));
+  t.deepEqual([1, 2, 3, 5, 6], result);
+});
+
+test("accept a promise of an iterable", async t => {
+  const iterable = [3, 1, 2, 6, 5].map(v => delay(v * 10).then(() => v));
+  const result = await concat.obj(asFullfills(Promise.resolve(iterable)));
   t.deepEqual([1, 2, 3, 5, 6], result);
 });
